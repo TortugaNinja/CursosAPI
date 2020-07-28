48ad45eb028c938e66f43d84f3b4c098d3cb59dd
TortugaNinja <alfonsina.barrancos@gmail.com>
2020-07-28 19:05:36 -0300

    parse enum to integer para solucionar error de postman

--- src/main/java/ar/com/ada/api/cursos/repositories/DocenteRepository.java
+++ src/main/java/ar/com/ada/api/cursos/repositories/DocenteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import ar.com.ada.api.cursos.entities.Docente;
 import ar.com.ada.api.cursos.entities.Pais.PaisEnum;
@@ -15,11 +16,11 @@ public interface DocenteRepository extends JpaRepository<Docente, Integer> {
     
      // jpql query, se está referenciando al obj docente, conviene para prevenir sql
     // injection
-    @Query("select 'true' from Docente d where d.pais_id=:pais and d.tipo_documento_id=:tipoDocuEnum and d.documento=:documento")
-    boolean existsDocente(PaisEnum pais, TipoDocuEnum tipoDocuEnum, String documento);
+    @Query("select CASE WHEN  count(d) > 0 THEN true ELSE false END from Docente d where d.paisId=:pais and d.tipoDocumentoId=:tipoDocuEnum and d.documento=:documento")
+    boolean existsDocente(Integer pais, Integer tipoDocuEnum, String documento);
 
     @Query("select d from Docente d where d.pais_id=:paisId and d.tipo_documento_id=:tipoDocuEnum and d.documento=:documento")
-    Docente buscarDocentePorDocu(PaisEnum pais, TipoDocuEnum tipoDocuEnum, String documento);
+    Docente buscarDocentePorDocu(Integer pais, Integer tipoDocuEnum, String documento);
 
     @Query("select d from Docente d order by nombre")
     List<Docente> obtenerListaDocentes();
